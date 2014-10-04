1a03ee216f36dca25f9fce918d3b11dd5f0a282a
Alex Vargas <vargas.84@osu.edu>
2014-10-04 13:17:24 -0400

    Add PoolProfile routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :users, only: [:show, :update, :edit, :destroy]
-  resources :pools
+
+  resources :pools do
+    resources :pool_profiles
+  end
+
   resources :pods, only: [:index, :show] do
     delete :leave
   end
