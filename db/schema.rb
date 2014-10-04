50aa006308008223d8a2a434cd9dd44c23eeeb54
Alex Vargas <vargas.84@osu.edu>
2014-10-03 22:17:13 -0400

    Create PoolProfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004020816) do
+ActiveRecord::Schema.define(version: 20141004021432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20141004020816) do
   end
 
   create_table "pods", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pool_profiles", force: true do |t|
+    t.text     "goals"
+    t.text     "skills"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
