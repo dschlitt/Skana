f5f523d6603ab4447a531d63bf40ed7ed3ce2043
Alex Vargas <vargas.84@osu.edu>
2014-10-04 02:52:40 -0400

    Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :update, :edit, :destroy]
+
+  respond_to :html
+
+  #def index
+    #@users = User.all
+    #respond_with @users
+  #end
+
+  def show
+    respond_with @user
+  end
+
+  #def new
+    #@user = User.new
+    #respond_with @user
+  #end
+
+  #def create
+    #@user = User.new user_params
+    #flash[:notice] = 'User was successfully created.' if @user.save
+    #respond_with @user
+  #end
+
+  def update
+    flash[:notice] = 'User was successfully updated.' if @user.save
+    respond_with @user
+  end
+
+  def edit
+    respond_with @user
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to :root
+  end
+
+  private
+
+  def set_user
+    @user = User.find params[:user_id]
+  end
+
+  def user_params
+    params.permit(:name)
+  end
+
+end
