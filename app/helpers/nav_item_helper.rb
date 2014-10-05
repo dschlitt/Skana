567477078490a88f1c024d4be57126832878e344
Alex Vargas <vargas.84@osu.edu>
2014-10-04 21:03:45 -0400

    fix path in nav_item helpers

--- app/helpers/nav_item_helper.rb
+++ app/helpers/nav_item_helper.rb
@@ -1,16 +1,16 @@
 module NavItemHelper
   def nav_item_chat alignment
-    ("<a href='/pods' class='btn btn-default btn-#{alignment}'>" +
+    ("<a href='#{pods_path}' class='btn btn-default btn-#{alignment}'>" +
      "<i class='fa fa-comments'></i></a>").html_safe
   end
 
   def nav_item_discover alignment
-    ("<a href='/pools' class='btn btn-default btn-#{alignment}'>" +
+    ("<a href='#{pools_path}' class='btn btn-default btn-#{alignment}'>" +
      "<i class='fa fa-search'></i></a>").html_safe
   end
 
   def nav_item_events alignment
-    ("<a href='/' class='btn btn-default btn-#{alignment}'>" +
+    ("<a href='#{root_path}' class='btn btn-default btn-#{alignment}'>" +
      "<i class='fa fa-user'></i></a>").html_safe
   end
 end
