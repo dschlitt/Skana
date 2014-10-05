2e5fa93b6e3b65d444601bf22249a0ae2bbece0f
Danny Schlitt <dschlitt2@gmail.com>
2014-10-04 21:35:28 -0400

    Mailgun Support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+
+  Action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    domain:               'skana.herokuapp.com',
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
