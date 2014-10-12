ef4f47f56a9008cec924a93e906e261ddc34e9ed
Alex Vargas <vargas.84@osu.edu>
2014-10-12 17:03:51 -0400

    Refactor matchmaker to notify client of matches

--- app/services/matchmaker.rb
+++ app/services/matchmaker.rb
@@ -2,32 +2,49 @@ module Matchmaker
   extend self
 
   def match(swipe, current_user)
-    render_value = nil
-
     swiped_profile = PoolProfile.find swipe.seen_pool_profile_id
     swiped_user = swiped_profile.user
 
-    if swipe.right_swipe # If current_user swiped right on swiped user
-      my_pool_profile = current_user.pool_profiles.find_by(pool_id: swipe.pool_id)
+    if has_match?(current_user, swiped_user, swipe)
 
-      has_match = swiped_user.swipes.exists?(
-        seen_pool_profile_id: my_pool_profile.id,
+      # initialize new pod
+      new_pod = Pod.new(pool_id: swipe.pool_id)
+
+      # grab old pods
+      my_pod = current_user.pods.find_by(pool_id: swipe.pool_id)
+      swiped_user_pod = swiped_user.pods.find_by(pool_id: swipe.pool_id)
+
+      # migrate to new pod (note: destroys old pods)
+      migrate_to_new_pod!(new_pod, my_pod, current_user, swiped_user_pod,
+                          swiped_user)
+
+      # save new pod
+      new_pod.save
+
+      # notify client of match
+      notify_client(new_pod)
+
+    end
+
+  end
+
+  private
+
+    def has_match?(current_user, swiped_user, swipe)
+      return false unless swipe.right_swipe
+      my_profile = current_user.pool_profiles.find_by(pool_id: swipe.pool_id)
+      swiped_user.swipes.exists?(
+        seen_pool_profile_id: my_profile.id,
         right_swipe: true
       )
+    end
 
-      if has_match # if swiped_user also swiped right on current_user
-
-        # grab matched pods
-        my_pod = current_user.pods.find_by(pool_id: swipe.pool_id)
-        swiped_user_pod = swiped_user.pods.find_by(pool_id: swipe.pool_id)
-
-        # move to new pod
-        new_pod = Pod.new(pool_id: swipe.pool_id)
-
+    def migrate_to_new_pod!(new_pod, my_pod, current_user, swiped_user_pod,
+                            swiped_user)
         if my_pod.present?
-          my_pod.messages.update_all pool_id: new_pod.id
+          my_pod.messages.update_all pod_id: new_pod.id
           new_pod.users << my_pod.users
-          pod.destroy
+          my_pod.destroy
         else
           new_pod.users << current_user
         end
@@ -39,35 +56,13 @@ module Matchmaker
         else
           new_pod.users << swiped_user
         end
-
-        # save new pod
-        new_pod.save
-
-        #respond to client
-        result = new_pod.attributes
-        puts "render match"
-        render_value = result.as_json
-
-      else # does not have match
-
-        @next_pp = PoolProfile.next(current_user, Pool.find(swipe.pool_id))
-        puts "\n\n\n\n\n"
-        puts "render next"
-        puts "\n\n\n\n\n"
-        render_value = @next_pp
-
-      end
-
-    else
-
-      @next_pp = PoolProfile.next(current_user, Pool.find(swipe.pool_id))
-      puts "\n\n\n\n\n"
-      puts "render next, swipe left"
-      puts "\n\n\n\n\n"
-      render_value = @next_pp
-
     end
 
-    render_value
-  end
+    def notify_client(new_pod)
+      result = new_pod.attributes
+      result["pool"] = new_pod.pool
+      channel = WebsocketRails["pool_#{new_pod.pool.id}"]
+      channel.trigger "matches.created", result.as_json
+    end
+
 end
