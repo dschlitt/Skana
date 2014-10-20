158cb7011364723ebcc1d51635741c21b9e6c51e
Alex Vargas <vargas.84@osu.edu>
2014-10-20 10:45:56 -0400

    Blacklist 'websocket' as id for friendly slug

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ Rails.application.routes.draw do
 
   resources :swipes, only: [:create]
 
-  get '/:id', to: 'pools#show'
+  get '/:id', to: 'pools#show', constraints: {id: /(?!websocket)/ }
 
 end
